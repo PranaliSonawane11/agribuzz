aae0d518564666d7c34fbf9cf0667b7bb1564f12
balaji <balajidontulofficial@outlook.com>
2021-05-20 16:12:06 +0530

    commit

--- displayproductsdetailed.php
+++ displayproductsdetailed.php
@@ -15,37 +15,37 @@ $rs = mysqli_fetch_array($qsql);
 ?>
 						<section>
 							<header>
-								<h2><?php echo $rs[product_name]; ?></h2>
+								<h2><?php echo $rs['product_name']; ?></h2>
 							</header>
                             
 <?php
 include("productslider.php");
 ?>
 <section>
-     <p><strong style="font-size:14px;">Cost :  <?php echo $rupeesymbol; ?><?php echo $rs[cost]; ?></strong> <strong></strong></p>
+     <p><strong style="font-size:14px;">Cost :  <?php echo $rupeesymbol; ?><?php echo $rs['cost']; ?></strong> <strong></strong></p>
 </section>
 <section>
     <header>
     <?php
-	if(isset($_SESSION[customerid]) || isset($_SESSION[sellerid]))
+	if(isset($_SESSION['customerid']) || isset($_SESSION['sellerid']))
 	{
 	?>
-        <h2><a href='displaycart.php?prodid=<?php echo $rs[0]; ?>&prodcost=<?php echo $rs[cost]; ?>'>Add to Cart</a></h2>
+        <h2><a href='displaycart.php?prodid=<?php echo $rs[0]; ?>&prodcost=<?php echo $rs['cost']; ?>'>Add to Cart</a></h2>
      <?php
 	}
 	else
 	{
 	?>
-<h2><a href='customerloginpanel.php?pagename=<?php echo basename($_SERVER['PHP_SELF']); ?>&productid=<?php echo $_GET[productid]; ?>'>Login to Customer panel</a></h2>
+<h2><a href='customerloginpanel.php?pagename=<?php echo basename($_SERVER['PHP_SELF']); ?>&productid=<?php echo $_GET['productid']; ?>'>Login to Customer panel</a></h2>
         <hr>
-<h2><a href='sellerloginpanel.php?pagename=<?php echo basename($_SERVER['PHP_SELF']); ?>&productid=<?php echo $_GET[productid]; ?>'>Login to Seller panel</a></h2><hr>
+<h2><a href='sellerloginpanel.php?pagename=<?php echo basename($_SERVER['PHP_SELF']); ?>&productid=<?php echo $_GET['productid']; ?>'>Login to Seller panel</a></h2><hr>
     <?php
 	}
 	?>
     </header>
 </section>
                            
-							<p><?php echo $rs[product_description]; ?></p>
+							<p><?php echo $rs['product_description']; ?></p>
 							
 						</section>
 					</div>
